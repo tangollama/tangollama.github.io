c8d62c12eb36e303b08433b4e23c11f120f30076
Joel Worrall <tangollama@gmail.com>
2017-10-13 21:32:13 -0400

    bug

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var name = "test_cookie";
 var value = "test_value";
 var expires = "";
 var date = new Date();
-date.setTime(date.getTime() + (days*24*60*60*1000));
+date.setTime(date.getTime() + (7*24*60*60*1000));
 expires = "; expires=" + date.toUTCString();
 document.cookie = name + "=" + value + expires + "; path=/";
 if (console != "undefined") {
