918c21015f3e2183628d80200888f57a60c5b6f0
Joel Worrall <tangollama@gmail.com>
2017-10-13 21:26:48 -0400

    console

--- test.js
+++ test.js
@@ -5,3 +5,7 @@ var date = new Date();
 date.setTime(date.getTime() + (days*24*60*60*1000));
 expires = "; expires=" + date.toUTCString();
 document.cookie = name + "=" + value + expires + "; path=/";
+if (console != "undefined") {
+  console.log("test.js");
+  console.log(document.cookie);
+}
