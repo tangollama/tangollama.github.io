9bed73a62e45492207e1d79caa8a7ed2155d2c4e
Joel Worrall <tangollama@gmail.com>
2018-02-28 20:30:38 -0500

    bacon

---
+++ cis191/ex/unit04/ex1/bacon.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+      if (results==null){
+        return null;
+      }
+      else{
+        return decodeURI(results[1]) || 0;
+      }
+  }
+
+  if ($.urlParam('jw') == 1) {
+    $('#defaultStyles').attr('href', 'http://joelworrall.com/cis191/ex/unit04/ex1/' + $('#defaultStyles').attr('href'));
+  }
+  if ($.urlParam('bacon') == 1) {
+    $('head').append('<link rel="stylesheet"  href="bacon.css" type="text/css" media="all">');
+  }
+});
