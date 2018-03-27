5827d53354d477a12c7423872b19826fdded23bb
Joel Worrall <tangollama@gmail.com>
2018-03-27 10:29:23 -0400

    added comments

--- cis191/ex/unit04/ex4/script.js
+++ cis191/ex/unit04/ex4/script.js
@@ -1,9 +1,15 @@
 function colorMe() {
+  /* 
+  this is a multi-lined
+  JavaScript comment
+
+  Doesn't it take up a lot of space?
+  */
   return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
 function baconPancakesSong() {
+  //this is a one line JavaScript comment related to a silly video
   window.open("https://www.youtube.com/watch?v=r57zosAFNw8");  
 }
 function adventureTime() {
-  alert("WE are now ready to make some bacon pancakes!!!");
 }
