66cfaed61a3a6cf78af2a52f98cf51db7dd29d64
tangollama <tangollama@gmail.com>
2021-10-01 23:29:41 -0400

    getSrc

--- src/components/BlogLink.js
+++ src/components/BlogLink.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
 import Card from "react-bootstrap/Card";
+import { getSrc } from "gatsby-plugin-image";
 
 export default (props) => {
   // console.debug(props);
   return (
     <Card className="card-container" as={Link} to={props.to}>
-      <Card.Img src={props.featuredImage} className="h-50" />
+      <Card.Img src={getSrc(props.featuredImage)} className="h-50" />
       <Card.Body className="pt-3">
         <Card.Title>
           <h4>{props.title}</h4>
